9f168791b812cd504ff94f86384f0c528816a9ec
rana <rana.ahammed.012@gmail.com>
2023-11-16 16:18:00 +0600

    database connected successfully

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDatabase from "./database/db.js";
 
-const app = express();
 dotenv.config();
+const app = express();
+connectDatabase();
 app.use(express.json());
 
 app.listen(process.env.PORT, () => {
-	console.log(`Server is running on ${process.env.PORT}`);
+	console.log(`Server is running on PORT ${process.env.PORT}`);
 });
