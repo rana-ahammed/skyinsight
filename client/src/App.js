c7c143f3f64258e284df0c93fbf8a08e6f24990f
rana <rana.ahammed.012@gmail.com>
2023-11-17 10:41:32 +0600

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
 	return (
 		<div className="bg-cyan-500 min-h-screen w-full">
-			<p>Hello World</p>
+			<Header />
+			<Outlet />
+			<Footer />
 		</div>
 	);
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	RouterProvider,
+	Route,
+} from "react-router-dom";
+import App from "./App";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<App />}>
+			<Route path="/" element={<Home />} />
+			<Route path="/login" element={<Login />} />
+		</Route>
+	)
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
 
