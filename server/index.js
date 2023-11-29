aa94a0a12884fed26bd39f0191926e3fb3b1600f
rana <rana.ahammed.012@gmail.com>
2023-11-29 23:37:05 +0600

    .

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import route from "./routes/weatherRoute.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 const corsOptions = {
 	origin: process.env.CLIENT_URL,
