e6c7f3769ea9445661f5d87fb67203952fcec5d1
rana <rana.ahammed.012@gmail.com>
2023-11-29 23:30:19 +0600

    .

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const corsOptions = {
-	origin: `${process.env.CLIENT_URL}`,
+	origin: process.env.CLIENT_URL,
 	credentials: true,
 };
 app.use(cors(corsOptions));
