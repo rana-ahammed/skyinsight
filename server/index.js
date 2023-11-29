3eb650ae11d2eb481b143f69c9718733d01b770c
rana <rana.ahammed.012@gmail.com>
2023-11-29 21:30:22 +0600

    .

--- server/index.js
+++ server/index.js
@@ -2,12 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
-import connectDatabase from "./database/db.js";
 import route from "./routes/weatherRoute.js";
 
 dotenv.config();
 const app = express();
-connectDatabase();
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
