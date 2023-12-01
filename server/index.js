330b335f57b5c5d0d01da19a8266732da5404465
rana <rana.ahammed.012@gmail.com>
2023-12-01 07:49:31 +0600

    .

--- server/index.js
+++ server/index.js
@@ -15,6 +15,14 @@ const corsOptions = {
 	origin: process.env.CLIENT_URL,
 	credentials: true,
 };
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
 app.use(cors(corsOptions));
 
 app.use("/", route);
