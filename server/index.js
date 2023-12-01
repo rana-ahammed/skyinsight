a0ec9546b0e2f9aaa599be93ecede10d49754761
rana <rana.ahammed.012@gmail.com>
2023-12-01 14:51:13 +0600

    .

--- server/index.js
+++ server/index.js
@@ -14,8 +14,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 app.use(function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Methods", "GET, POST");
+	res.header("Access-Control-Allow-Origin", `${process.env.CLIENT_URL}`);
+	res.header("Access-Control-Allow-Credentials", true);
+	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
 	next();
 });
 
