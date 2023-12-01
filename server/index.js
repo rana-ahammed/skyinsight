ef23c570c311fe84bb79d77d8d94462ac3ba6286
rana <rana.ahammed.012@gmail.com>
2023-12-01 20:46:37 +0600

    .

--- server/index.js
+++ server/index.js
@@ -15,16 +15,6 @@ const corsConfig = {
 	origin: process.env.CLIENT_URL,
 	credentials: true,
 	methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
-	allowedHeaders: [
-		"Content-Type",
-		"Origin",
-		"X-Requested-With",
-		"Accept",
-		"x-client-key",
-		"x-client-token",
-		"x-client-secret",
-		"Authorization",
-	],
 };
 
 app.use(cors(corsConfig));
