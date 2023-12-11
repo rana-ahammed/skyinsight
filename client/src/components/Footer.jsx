075794dfacc813e54936a32ff9b3b0b05b6d0c0f
rana <rana.ahammed.012@gmail.com>
2023-12-11 22:02:54 +0600

    .

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
 	return (
 		<footer className="bg-gray-300 dark:bg-slate-800 p-3 w-full flex flex-col md:flex-row items-center justify-between">
 			<p className="text-sm md:text-lg mb-2 font-semibold text-center text-black dark:text-white w-1/2">
-				All Rights Reserved By Weathery
+				All Rights Reserved By SkyInsight
 			</p>
 			<section className="flex items-center justify-center gap-5 text-2xl text-black dark:text-white w-1/2">
 				<a
